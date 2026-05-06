418b27767dc8215c7acc643bec684748b4ca506e
rishhh112004 <rishabhupadhyay112004@gmail.com>
2026-05-06 11:23:54 +0530

    Removed unwanted files

--- backend/test_text_processing.py
+++
@@ -1,7 +0,0 @@
-from backend.services.text_processing import process_uploads
-chunks = process_uploads()
-for c in chunks:
-    print("\n--- CHUNK ---")
-    print("Upload ID:", c["upload_id"])
-    print("Timestamp:", c["timestamp"])
-    print("Text:", c["chunk_text"])
--- run_chatbot.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host ""
-Write-Host "Starting Offline RAG Chatbot..."
-Write-Host ""
-
-# Activate virtual environment
-& .\venv\Scripts\Activate.ps1
-
-# Run chatbot
-python backend/terminal_chat.py
